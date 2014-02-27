80e340334d89d643b0052892b628a787ceadfee0
Heath Borders <heath.borders@jivesoftware.com>
2014-02-27 02:03:39 -0600

    Added simple aar dependency

--- android-maven-talk-apk/src/main/java/com/github/hborders/android/maven/HelloAndroidActivity.java
+++ android-maven-talk-apk/src/main/java/com/github/hborders/android/maven/HelloAndroidActivity.java
@@ -2,21 +2,41 @@ package com.github.hborders.android.maven;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
+import it.sephiroth.android.library.ab.AB;
+import it.sephiroth.android.library.ab.ABTest;
 
 public class HelloAndroidActivity extends Activity {
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
+     * @param savedInstanceState If the activity is being re-initialized after
+     * previously being shut down then this Bundle contains the data it most
      * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AB alphaBetaTest = AB.getInstance( this );
+        alphaBetaTest.doABTest( "test-action", new ABTest() {
+            @Override
+            public void A() {
+                Toast.makeText(HelloAndroidActivity.this, "A", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void B() {
+                Toast.makeText(HelloAndroidActivity.this, "B", Toast.LENGTH_LONG).show();
+            }
+        } );
     }
 
     @Override
